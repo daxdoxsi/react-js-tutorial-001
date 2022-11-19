0d0e258754e1f92a1353a92f2328e595ddaea861
David M. Miller <daxdoxsi@gmail.com>
2022-11-19 03:48:03 -0600

    Using states to update UI content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from "react";
 function App() {
+    const [myCounter,setMyCounter] = useState(0);
+
+    function handleClick() {
+        setMyCounter(myCounter + 1);
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello click #{myCounter}</h1>
+        <button onClick={handleClick}>== {myCounter} ==</button>
     </div>
   );
 }
